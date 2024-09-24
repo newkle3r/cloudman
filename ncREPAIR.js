366eb2e83e031eb801f567b106daef0e2580966b
newkle3r <kontakt@hannesahlin.se>
2024-09-24 04:46:36 +0200

    minor spelling

--- ncREPAIR.js
+++ ncREPAIR.js
@@ -97,7 +97,7 @@ class ncREPAIR {
         const spinner = createSpinner('Repairing Nextcloud...').start();
         
         try {
-            awaitthis.runCommand(`${this.NC_OCC} maintenance:repair`);
+            await this.runCommand(`${this.NC_OCC} maintenance:repair`);
             spinner.success({ text: `${GREEN('Nextcloud has been repaired!')}` });
         } catch (error) {
             spinner.error({ text: `${RED('Failed to repair Nextcloud')}` });
@@ -113,7 +113,7 @@ class ncREPAIR {
     async autoRepair() {
       clearConsole()
         console.log('Starting auto-repair process...');
-        awaitthis.runCommand(`bash ${this.SCRIPTS_PATH}/repair.sh`);
+        this.runCommand(`bash ${this.SCRIPTS_PATH}/repair.sh`);
         console.log('Auto-repair completed.');
         await this.awaitContinue();
     }
@@ -195,7 +195,7 @@ class ncREPAIR {
 
     try {
         const spinner = createSpinner(`Downloading Nextcloud ${versionNumber} binaries...`).start();
-        awaitthis.runCommand(`curl -o ${homeDir}/nextcloud-${versionNumber}.zip ${downloadUrl}`);
+        this.runCommand(`curl -o ${homeDir}/nextcloud-${versionNumber}.zip ${downloadUrl}`);
         spinner.success({ text: `Nextcloud version ${versionNumber} binaries downloaded!` });
     } catch (error) {
         console.error(`Failed to download Nextcloud version ${versionNumber} binaries:`, error);
@@ -261,7 +261,7 @@ class ncREPAIR {
         console.log(`Unzipping ${zipFilePath}...`);
         try {
             const spinner = createSpinner('Unzipping binaries...').start();
-            awaitthis.runCommand(`unzip -q ${zipFilePath} -d ${unzipDirPath}`);
+            this.runCommand(`unzip -q ${zipFilePath} -d ${unzipDirPath}`);
             spinner.success({ text: 'Unzipping completed.' });
         } catch (error) {
             console.error('Failed to unzip the Nextcloud binaries:', error);
@@ -308,15 +308,15 @@ class ncREPAIR {
 
       if (!fs.existsSync(this.BACKUP_PATH)) {
           console.log(`${this.BACKUP_PATH} does not exist. Creating backup directory...`);
-          awaitthis.runCommand(`sudo mkdir -p ${this.BACKUP_PATH}`);
+          this.runCommand(`sudo mkdir -p ${this.BACKUP_PATH}`);
       }
 
       console.log('Backing up current Nextcloud data...');
 
       try {
           const spinner = createSpinner('Backing up...').start();
-          awaitthis.runCommand(`sudo rsync -Aax ${this.NC_PATH}/config ${this.BACKUP_PATH}/`);
-          awaitthis.runCommand(`sudo rsync -Aax ${this.NC_PATH}/apps ${this.BACKUP_PATH}/`);
+          this.runCommand(`sudo rsync -Aax ${this.NC_PATH}/config ${this.BACKUP_PATH}/`);
+          this.runCommand(`sudo rsync -Aax ${this.NC_PATH}/apps ${this.BACKUP_PATH}/`);
           spinner.success({ text: 'Backup completed successfully!' });
       } catch (error) {
           console.error('Failed to backup Nextcloud data:', error);
@@ -466,10 +466,10 @@ class ncREPAIR {
     async installDependencies() {
         const dependencies = ['curl', 'whiptail', 'lshw', 'net-tools', 'bash-completion', 'cron'];
         for (const dep of dependencies) {
-            const isInstalled = awaitthis.runCommand(`dpkg-query -W -f='${dep}' 2>/dev/null | grep -c "ok installed"`);
+            const isInstalled = this.runCommand(`dpkg-query -W -f='${dep}' 2>/dev/null | grep -c "ok installed"`);
             if (isInstalled !== '1') {
                 console.log(`Installing ${dep}...`);
-                awaitthis.runCommand(`apt-get install ${dep} -y`);  // Install dependency
+                this.runCommand(`apt-get install ${dep} -y`);  // Install dependency
             } else {
                 console.log(`${dep} is already installed.`);
             }
